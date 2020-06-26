9210544b01e70475356cc668db2737cadd63bbf3
Freya <freya@stubkjaer.co.uk>
2020-06-26 10:17:40 -0700

    Adds index view to / path and registers app

--- django_project_name/urls.py
+++ django_project_name/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_app_name import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='Homepage')
 ]
