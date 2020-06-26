d77fa51cd4a03ead270a2ab225f11aeab72c282d
Freya <freya@stubkjaer.co.uk>
2020-06-26 10:51:04 -0700

    Adds configuration for django_heroku

--- django_project_name/settings.py
+++ django_project_name/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django-heroku.settings(locals())
