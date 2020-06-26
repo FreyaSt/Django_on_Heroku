9111ef86f5f6ef40eefe292da6f5b3dfc5845506
Freya <freya@stubkjaer.co.uk>
2020-06-26 10:54:34 -0700

    Fixes typo in django_heroku config

--- django_project_name/settings.py
+++ django_project_name/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django-heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-django-heroku.settings(locals())
+django_heroku.settings(locals())
